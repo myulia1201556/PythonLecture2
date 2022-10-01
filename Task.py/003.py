14060b4c3938b73007173b0b5748980742155ac8
Yulia <yulia1201556@gmail.com>
2022-10-01 19:53:37 +0300

    Сумма чисел из списка заданного в определённой последовательности

--- Task.py/003.py
+++ Task.py/003.py
@@ -1,9 +1,9 @@
 # Задайте список из n чисел последовательности 
 # (1+1/n)^n и выведите на экран их сумму.
 
-list = []
+print ("Введите размер списка n: ")
 n = int(input())
-sum = 0
+  
+list = [round((1+1/i)**i, 3) for i in range(1, n+1)]
 
-for i in range (1, n + 1):
-    
+print(f"{list} \nСумма: {round(sum(list), 3)}")
